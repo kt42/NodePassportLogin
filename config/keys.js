90351063fefe8fbb19c8449cf4ed3a63ac090cd1
Kevin Teeling <kteeling42@gmail.com>
2021-03-09 17:23:00 +0000

    test 1

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
 module.exports = {
-    mongoURI: dbPassword
+    mongoURI: 'mongodb+srv://keva06:FreshPass123@cluster0.sjjh8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+
 };
